4c565262c1fe35aee7a2b745831a9feec55deb54
Anastasiya <anastasiya.koshelevskaya@nova-test.ru>
2019-03-13 18:54:51 +0300

    animate

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,13 @@ import documentReady from './utils/documentReady';
 import documentLoaded from './utils/documentLoaded';
 import sitePreloader from './modules/site-preloader';
 import slider from './modules/sliders';
+import AOS from 'aos'
 
 
 documentReady(() => {
   sitePreloader.init();
   slider.init();
+  AOS.init();
 });
 
 
